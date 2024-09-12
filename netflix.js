59018b883caee0139cd9072cb85a311ba13f566b
Awino <estherojul53@gmail.com>
2024-09-12 13:04:36 +0300

    click

--- netflix.js
+++ netflix.js
@@ -1,9 +1,12 @@
 //variable
 
-const accordion= document.getElementsByClassName("content-container");
+const accordion= document.getElementsByClassName("question");
 for(i=0; i< accordion.length; i++){
-    accordion[i].addEventListener("click", function(){
-        this.classList.toggle("active");
+    accordion[i].addEventListener("click", function(e){
+        document.querySelectorAll(".content-container").forEach(cont=>{
+            cont.classList.remove("active")? cont.classList.remove("active"):""
+        })
+        e.target.parentElement.classList.toggle("active");
         
     })
 };
